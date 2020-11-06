1f117e0a47d283958c211210258153585d4ba5d6
himcs <787673628@qq.com>
2020-11-06 18:01:12 +0800

    1

--- src/main/java/io/himcs/sdk/action/ProjectAction.java
+++ src/main/java/io/himcs/sdk/action/ProjectAction.java
@@ -5,14 +5,9 @@ import com.intellij.database.model.DasNamed;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vfs.VirtualFile;
 import io.himcs.sdk.util.MDbUtil;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class ProjectAction extends AnAction {
 
